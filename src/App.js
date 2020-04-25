df9a602b55e0aa102742391e12a05e41d82ba650
Vitor Soares <vitorcabeca2005@hotmail.com>
2020-04-25 19:59:03 -0300

    Reactotron Configured

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
 import Routes from './routes';
-import GlobalStyle from './styles/global';
 import history from './services/history';
+
+import GlobalStyle from './styles/global';
 
 function App() {
     return (
